0d13d02195715f4b3b341040bad533a1bc61de46
rHistel <r.kasper@mission-webstyle.de>
2021-04-23 12:57:20 +0200

    DiceHelper wird durch die DiceActivity angesteuert und genutzt.

--- LassDieWuerfelRollen/app/src/main/java/de/rhistel/lassdiewuerfelrollen/gui/DiceActivity.kt
+++ LassDieWuerfelRollen/app/src/main/java/de/rhistel/lassdiewuerfelrollen/gui/DiceActivity.kt
@@ -2,8 +2,12 @@ package de.rhistel.lassdiewuerfelrollen.gui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
+import android.widget.TextView
 import de.rhistel.lassdiewuerfelrollen.R
+import de.rhistel.lassdiewuerfelrollen.logic.DiceHelper
+import de.rhistel.lassdiewuerfelrollen.settings.*
 
 /**
  * Diese Activity zeigt fuenf Wuerfel an,
@@ -14,40 +18,80 @@ import de.rhistel.lassdiewuerfelrollen.R
  */
 class DiceActivity : AppCompatActivity() {
 
-    //region 1. Decl. and Init Attribute / Widgets
+	//region 1. Decl. and Init Attribute / Widgets
 
-    /**
-     * [ImageView]s welche die Wuerfel
-     * und deren randomisierte Augenzahl abbilden
-     */
-    private val imgvsDice by lazy {
-        arrayOf<ImageView>(
-            this.findViewById(R.id.imgvFirstDie),
-            this.findViewById(R.id.imgvSecondDie),
-            this.findViewById(R.id.imgvThirdDie),
-            this.findViewById(R.id.imgvFourthDie),
-            this.findViewById(R.id.imgvFifthDie)
-        )
-    }
-    //endregion
+	/**
+	 *[TextView] zum Anzeigen des Ergebnises des Wurfes
+	 * welcher beim Klick auf den Button [btnRollTheDice] gestartet wird.
+	 */
+	private val txtvRollResult by lazy { findViewById<TextView>(R.id.txtvRollResult) }
 
-    //region 2. Lebenszyklus
+	/**
+	 *Startet das Wuerfeln
+	 */
+	private val btnRollTheDice by lazy { findViewById<Button>(R.id.btnRollTheDice) }
 
-    /**
-     * Startet als erstes nach dem Start der Actvitiy
-     */
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+	/**
+	 * [ImageView]s welche die Wuerfel
+	 * und deren randomisierte Augenzahl abbilden
+	 */
+	private val imgvsDice by lazy {
+		arrayOf<ImageView>(
+			this.findViewById(R.id.imgvFirstDie),
+			this.findViewById(R.id.imgvSecondDie),
+			this.findViewById(R.id.imgvThirdDie),
+			this.findViewById(R.id.imgvFourthDie),
+			this.findViewById(R.id.imgvFifthDie)
+		)
+	}
+	//endregion
 
-        //1. Layout setzen
-        this.setContentView(R.layout.dice_activity_layout)
+	//region 2. Lebenszyklus
 
-        //2. Widgets generieren
+	/**
+	 * Startet als erstes nach dem Start der Actvitiy
+	 */
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
 
-        for (imgvCurrentDie in this.imgvsDice) {
-            imgvCurrentDie.setImageResource(R.drawable.six_pips)
-        }
+		//1. Layout setzen
+		this.setContentView(R.layout.dice_activity_layout)
 
-    }
-    //endregion
+	}
+	//endregion
+
+
+	//region 3. Klickhandling
+	/**
+	 * Diese Methode Wuerfelt und zeigt das
+	 * Ergebnis des Wurfes auf [txtvRollResult] an.
+	 */
+	fun onClickRollTheDice() {
+
+		//Wuerfeln zufaellige Augenzahl
+		val allDice = DiceHelper.rollDice();
+
+		//Alle ImageViews und Wuerfel durchlaufen beide Arrays sind gleich groß
+		for (index in this.imgvsDice.indices) {
+
+			//Akutelle Augenzahl auswerten und passende BildId zuordnen
+			val matchingPipDrawableId = when (allDice[index]) {
+				ONE_PIP -> R.drawable.one_pip
+				TWO_PIPS -> R.drawable.two_pips
+				THREE_PIPS -> R.drawable.three_pips
+				FOUR_PIPS -> R.drawable.four_pips
+				FIVE_PIPS -> R.drawable.five_pips
+				SIX_PIPS -> R.drawable.six_pips
+				else       -> R.drawable.six_pips
+			}
+
+			//In akuteller Wuerfel ImageView die Bild mit passender Augenanzahl anzeigen
+			this.imgvsDice[index].setImageResource(matchingPipDrawableId);
+
+		}
+
+		//Wurergebenis anzeigen
+		this.txtvRollResult.text = DiceHelper.evaluateDice(this,allDice);
+	}
+	//endregion
 }
